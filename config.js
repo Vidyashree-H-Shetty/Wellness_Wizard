cbe1b01572a0f34a4a06af659ee0e66ce2076123
Vidyashree H Shetty <147435709+Vidyashree-H-Shetty@users.noreply.github.com>
2025-03-22 18:17:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,18 @@
+//USE THIS WHILE WORKING IN DEVELOPMENT MODE
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const isProduction = process.env.NODE_ENV === 'production';
+
+// const pool = new Pool({
+//   connectionString: isProduction ? process.env.DATABASE_URL : `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+//   ssl: isProduction ? { rejectUnauthorized: false } : false, // Ensure compatibility with Render's database
+// });
+
+// module.exports = { pool };
+
+
+//USE THIS WHILE WORKING IN PRODUCTION MODE(DEPLOYMENT)
 require('dotenv').config();
 const { Pool } = require('pg');
 
